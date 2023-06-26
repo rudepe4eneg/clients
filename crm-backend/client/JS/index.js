7d4ae0409ed61ac4f0ba7ebc69db54d4ac76e6c0
Daniil Zaytsev <daniil_zaytsev_02@mail.ru>
2023-06-27 00:06:49 +0300

    aa

---
+++ crm-backend/client/JS/index.js
@@ -0,0 +1,33 @@
+import { createClientsHeader } from "./createHeader.js";
+import { createClientsSection } from "./createClientsSection.js";
+import { getClients } from "./clientsApi.js";
+import { createClientItem } from "./createClientItem.js";
+import { sortTable } from "./sortClientsTable.js";
+import { searchClients } from "./searchClient.js";
+
+const createApp = async () => {
+    const header = createClientsHeader();
+    const clientSection = createClientsSection();
+    document.body.append(header, clientSection.main);
+    const preloader = document.querySelector('.preloader');
+    const tableWrapper = document.querySelector('.clients__wrapper');
+
+    try {
+        tableWrapper.style.overflow = 'visible';
+        const clients = await getClients();
+        searchClients(clients);
+
+        for (const client of clients) {
+            document.querySelector('.clients__tbody').append(createClientItem(client))
+        }
+    } catch (error) {
+        console.log(error);
+    } finally {
+        preloader.remove()
+        tableWrapper.style.overflow = 'auto';
+    }
+
+}
+
+createApp();
+document.addEventListener('DOMContentLoaded', sortTable);
